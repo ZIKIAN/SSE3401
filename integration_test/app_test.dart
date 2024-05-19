f1b7ab6ceb0303dd37a5acf1cce8da9f55e5b582
User <217064@student.upm.edu.my>
2024-05-19 15:20:35 +0800

    yyhh

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -4,7 +4,7 @@ import 'package:lab5/factory1.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:lab5/main.dart' as app;
 import 'package:lab5/activation.dart';
-import 'package:lab5/invitation.dart';
+import 'package:lab5/otp.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
@@ -32,25 +32,25 @@ void main() {
         expect(find.byType(Activation), findsOneWidget);
 
         await tester.enterText(find.byType(TextField), "537737");
+        await tester.pumpAndSettle();
         await Future.delayed(const Duration(seconds: 3));
         await tester.tap(find.byType(Checkbox));
+        await tester.pumpAndSettle();
         await Future.delayed(const Duration(seconds: 2));
         await tester.tap(find.byType(ElevatedButton));
         await tester.pumpAndSettle();
 
 
-        expect(find.byType(Invitation), findsOneWidget);
+        expect(find.byType(OTP), findsOneWidget);
 
         await Future.delayed(const Duration(seconds: 2));
-        await tester.enterText(find.byKey(const Key("Text field")),'TANG');
+        await tester.enterText(find.byType(TextField), "521130");
         await tester.pumpAndSettle();
         await Future.delayed(const Duration(seconds: 4));
-        await tester.enterText(find.byKey(const Key("Text field")), "451651848");
+        await tester.tap(find.byType(ElevatedButton));
         await Future.delayed(const Duration(seconds: 5));
         await tester.pumpAndSettle();
-        await tester.tap(find.byKey(const Key("button")),);
-        await tester.pumpAndSettle();
-        await Future.delayed(const Duration(seconds: 3));
+
 
         Navigator.of(tester.element(find.byType(Scaffold))).pop();
         await tester.pumpAndSettle();
